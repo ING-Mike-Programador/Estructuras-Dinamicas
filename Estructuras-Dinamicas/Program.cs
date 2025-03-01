16bdf61387dd445bcbfda662cdcfd43c3e8625d3
ING-Mike-Programador <miguel_alonso44@hotmail.com>
2025-02-28 18:30:01 -0700

    Cambio la ubicacion de la clase pila abajo de program por problemas de interacción entre clases

--- Estructuras-Dinamicas/Program.cs
+++ Estructuras-Dinamicas/Program.cs
@@ -8,21 +8,65 @@ namespace Estructuras_Dinamicas
 {
     internal class Program
     {
+        
         static void Main(string[] args)
         {
-            String nombre = "";
-            String Nombre = Console.ReadLine();
+           
+            objeto.setNombre("Hola mundo");
+
+            Console.WriteLine(objeto.getNombre());
+            Console.ReadKey();
+        }
+    }
+    
+    public class objetoPila
+    {
+        // variables //
+        private objetoPila objSig = null; // Variable privada para asimilar al siguiente componente que vendra //
+        private string nombre = ""; // Variable nombre //
+        private string edad = "0"; // Variable edad //
+        private string genero = ""; // Variable genero //
+
+        // Constructor de la clase //
+        public objetoPila()
+        {
+        }
+
+
+        // Metodos //
+        public void setNombre(string Nombre)  // Asignar nombre //
+        {
             if (Nombre != null && Nombre.Trim() != "")
             {
-                nombre = Nombre;
+                nombre = Nombre.Trim();
             }
             else
             {
                 nombre = "Desconocido";
             }
-
-            Console.WriteLine(nombre);
-            Console.ReadKey();
         }
+        public void setEdad(int Edad) // Asignar edad //
+        {
+            if (Edad != null && Edad > 0)
+            {
+                edad = Edad.ToString();
+            }
+            else
+            {
+                edad = "Desconocida";
+            }
+        }
+        public void setGenero(string Genero) // Asignar genero //
+        {
+            if (Genero != null && Genero.Trim() != "")
+            {
+                genero = Genero.Trim();
+            }
+            else
+            {
+                genero = "Otro";
+            }
+        }
+        public string getNombre() => nombre;
     }
 }
